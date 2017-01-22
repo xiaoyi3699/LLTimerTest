0f5d7134b9bc248529f8460d5c52165e07a32d17
wangzhaomeng <122589615@qq.com>
2017-01-22 17:34:51 +0800

    first commit

---
+++ timer/LLTest.h
@@ -0,0 +1,13 @@
+//
+//  LLTest.h
+//  timer
+//
+//  Created by zhaomengWang on 17/1/22.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLTest : NSObject
+
+@end
---
+++ timer/LLTest.m
@@ -0,0 +1,13 @@
+//
+//  LLTest.m
+//  timer
+//
+//  Created by zhaomengWang on 17/1/22.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "LLTest.h"
+
+@implementation LLTest
+
+@end
